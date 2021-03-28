4db32b408d9fee056cbf556ddc17e9a5dd235517
Pavel Kalganov <piano@Pianos-MBP.netis>
2021-03-28 20:47:09 +0400

    big fix

--- js/main.js
+++ js/main.js
@@ -1,26 +1,26 @@
-new Glider(document.querySelector(".glider"), {
-  slidesToShow: 6,
-  dots: "#dots",
-  arrows: {
-    prev: ".review__arrow-button--left",
-    next: ".review__arrow-button--right",
-  },
-  exactWidth: false,
-  scrollLock: true,
-  responsive: [
-    {
-      breakpoint: 900,
-      settings: {
-        slidesToShow: 3,
-        slidesToScroll: 1,
+new Glider(document.querySelector('.glider'), {
+    dots: '#dots',
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    scrollLock: true,
+    responsive: [
+      {
+        breakpoint: 720,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
       },
-    },
-    {
-      breakpoint: 616,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
+      {
+        breakpoint: 616,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
       },
-    },
-  ],
-});
+    ],
+    arrows: {
+      prev: '.review__arrow-button--left',
+      next: '.review__arrow-button--right'
+    }
+  });
